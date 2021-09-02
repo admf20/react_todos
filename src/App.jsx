3cc6966371d0935fe77ac62b6d356de782ed7897
andres20001 <felipe20001monsalve@gmail.com>
2021-09-02 01:24:54 -0500

    documente los diferentes conceptos de react y funciones creadas

--- src/App.jsx
+++ src/App.jsx
@@ -9,41 +9,46 @@ export function App (){
     const todoTaskRef = useRef();
     const [todos, setTodos] = useState([])
 
-    useEffect(() => {
+    useEffect(() => { //TODO:aca traemos los datos desde el localStorage 
         const storedTodos = JSON.parse(localStorage.getItem(KEY));
-        if(storedTodos){
+        if(storedTodos){ //TODO:validamos si hay algo en el localStorage
             setTodos(storedTodos)
         }
     }, [])
 
-    useEffect(() => {
+    useEffect(() => {//TODO:guardamos los datos en el localStorage
         localStorage.setItem(KEY, JSON.stringify(todos))
     }, [todos])
 
-    const toggleTodo = (id) => {
+    const toggleTodo = (id) => { //TODO:funcion para cambiar el estado del completed que cambia el checkbox
         const newTodos = [...todos];
         const todo = newTodos.find((todo) => todo.id === id);
         todo.completed = !todo.completed;
         setTodos(newTodos);
     }
 
-    const handleTodoAdd = () => {
-        const task = todoTaskRef.current.value;
-        if(task === '') return;
+    const handleTodoAdd = () => { //TODO:funcion para guardar una nueva nota
+        const task = todoTaskRef.current.value; //TODO:por medio del hooks 'useRef' podemos tener acceso al input
+                                                //y en este caso ver cual es su valor con 'value'
+        if(task === '') return;  
 
         setTodos((prevTodos) => {
             return [...prevTodos, {id: uuidv4(), task, completed: false}];
         });
 
         todoTaskRef.current.value = null;
+        todoTaskRef.current.focus();
     }
 
-    const handleClearAll = () =>{
+    const handleClearAll = () =>{//TODO:funcion para eliminar las tareas que ya estan el ckekbox completado 
         const newTodos = todos.filter((todo) => !todo.completed);
         setTodos(newTodos);
+
+        todoTaskRef.current.focus();
     }
 
     return (
+    ///TODO: le enviamos los props al componente 'TodoList' con el nombre todos
     <Fragment>
         <TodoList todos={todos} toggleTodo={toggleTodo}/>
         <input ref={todoTaskRef} type="text" placeholder="Nueva Tarea" />
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,7 +4,8 @@ import { TodoItem } from './TodoItem'
 export function TodoList({todos, toggleTodo}) {
     return (
         <ul>
-            {todos.map((todo) => (
+            {todos.map((todo) => (///TODO: al recibir el props en el cual estan llamados como 'todos' desde App hacemos un mapeo y se lo 
+                                 //pasamos el componente 'TodoItem'
                 <TodoItem 
                     key={todo.id}
                     todo={todo}
